f94bb6fc7f70e4d9e21d6432cabf56d45b8a07cc
RealityDreamsStudio <reality.dreams.studio@gmail.com>
2023-01-04 13:46:19 -0300

    1º Update - 04/01/2019
    Upload de todos os arquivos do projeto.

---
+++ Fecart - Escape from the Earth/Scripts/Game.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+onready var cena = get_tree().get_current_scene()
+
+# Referencias dos nodes
+var alien
+var timer
+var label
+
+# Atributos do jogo
+var pontos = 0
+var estado = 1
+ 
+const JOGANDO = 1
+const PERDENDO = 2
+
+func _ready():
+	# Obtem o node inicial o jogo
+	alien = get_node("Alien")
+	timer = get_node("Timer")
+	label = get_node("Control/Label")
+
+func kill():
+	alien.apply_impulse(Vector2(0,0), Vector2(-1000,0))
+	get_node("AnimationPlayer").stop()
+	estado = PERDENDO
+	timer.start()
+
+func _on_Timer_timeout():
+	get_tree().reload_current_scene()
+	print("fim")
+
+func pontuar():
+	pontos += 1
+	label.set_text(str(pontos))
+	print(pontos)
+
+func _on_Area2D_body_entered(body):
+	if body.get_name() == "Alien" || body.get_name() == "Limite":
+		cena.kill()
+	
---
+++ Fecart - Escape from the Earth/Scripts/Jato.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+# Referencia a cena do game
+onready var cena = get_tree().get_current_scene()
+
+export var vel = 150
+export var limite = -100
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	
+	# Mover o cano
+	position.x -= vel * delta
+	
+	# Retornar e sortear uma altura em Y
+	if position.x < limite:
+		position = Vector2(3700, randi()%500+100)
+
+func _on_Area2D_body_entered(body):
+	# Verifica o nome 
+	if body.get_name() == "Alien":
+		cena.kill()
+
+func _on_Area2D2__Pontos_body_entered(body):
+	if body.get_name() == "Alien":
+		cena.pontuar()
+
+func _on_Area2D3__Pontos_body_entered(body):
+	if body.get_name() == "Alien":
+		cena.pontuar()
